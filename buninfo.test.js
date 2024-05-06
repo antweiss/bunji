13e9363c15169104a9be155acd948897ecf91976
Ant Weiss <ant.weiss@gmail.com>
2024-05-06 14:05:43 +0300

    Update buninfo.test.js

--- buninfo.test.js
+++ buninfo.test.js
@@ -4,16 +4,16 @@ import { getMem, getCPU } from "./info.js";
 test("checkCPU", async () => {
   const data = await getCPU();
   expect(data.cores).toBeGreaterThan(0);
-}, 50);
+}, 60);
 
 test("checkMem", async () => {
   const data = await getMem();
   expect(data.total).toBeGreaterThan(0);
-}, 50);
+}, 60);
 
 test("checkFreeMem", async () => {
   const data = await getMem();
   //replace this with expect(data.total).toBeGreaterThan(data.free);
   //for the test to succeed
   expect(data.total).toBeGreaterThan(data.free);
-}, 50);
+}, 60);
